7fa4c5ad53cbb5ccade6aef750c84504b567537a
Eric Lobdell <eric@ericlobdell.me>
2017-12-13 18:45:04 -0500

    more custom error handling work

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,10 @@ enum Direction {
 
 #[derive(PartialEq,Debug)]
 enum MovementError {
-    NoBeingInSquare
+    NoBeingInSquare,
+    SquareOffGrid,
+    SquareOccupied,
+    StoneTerrain
 }
 
 impl Grid {
@@ -45,8 +48,20 @@ impl Grid {
         let square = self.squares.get(coord.0 * self.size.0 + coord.1)
             .expect("Index out of bounds trying to get being");
 
+        let destcoords = match dir {
+            Direction::West => (coord.0 - 1, coord.1),
+            Direction::East => (coord.0 + 1, coord.1),
+            Direction::North => (coord.0, coord.1 - 1),
+            Direction::South => (coord.0, coord.1 + 1)
+        };
+        
         match square.being {
-            Some(_) => Ok((0,0)),
+            Some(_) => match self.squares.get(destcoords.0 * self.size.0 + destcoords.1) {
+                Some(s) if s.ground == TerrainGround::Stone => Err(MovementError::StoneTerrain),
+                Some(s) if s.being != None => Err(MovementError::SquareOccupied),
+                Some(_) => Ok(destcoords),
+                None => Err(MovementError::SquareOffGrid)
+            },
             None => Err(MovementError::NoBeingInSquare)
         }
     }
@@ -80,13 +95,40 @@ mod tests {
         for square in &grid.squares {
             assert_eq!(square.ground, ::TerrainGround::Soil);
             assert_eq!(square.block, None);
-            assert_eq!(square.beings, None);
+            assert_eq!(square.being, None);
             number_of_squares += 1;
         }
 
         assert_eq!(grid.squares.len(), 5*13);
         assert_eq!(number_of_squares, 5*13);
     }
+
+    #[test]
+    fn test_move_being_not_in_square() {
+        let grid = ::Grid::generate_empty(3, 3);
+        assert_eq!(grid.move_being_in_coord((0, 0), ::Direction::West), Err(::MovementError::NoBeingInSquare));
+    }   
+
+    #[test]
+    fn test_move_being_to_stone_square() {
+        let grid = ::Grid::generate_empty(3, 3);
+        
+        let playerSquare = ::Square {
+            ground: ::TerrainGround::Soil,  
+            block: Some(::TerrainBlock::Soil),
+            being: Some(::Being::Human)
+        };
+        let stoneSqure = ::Square { 
+            ground: ::TerrainGround::Stone,  
+            block: None,
+            being: None
+        };
+
+        grid.squares[0] = stoneSqure;
+        grid.squares[1] = playerSquare;
+
+        assert_eq!(grid.move_being_in_coord((0, 1), ::Direction::East), Err(::MovementError::StoneTerrain));
+    }   
 }
 
 fn main() {
